d59a9512de1283d7553df7ebfe583900f3457f83
Axel Fernandez <axel.fernandez@mercadolibre.com>
2021-08-09 22:10:22 -0300

    Stable Version

--- app/src/main/java/com/axelfernandez/streaming/MainActivity.kt
+++ app/src/main/java/com/axelfernandez/streaming/MainActivity.kt
@@ -3,6 +3,7 @@ package com.axelfernandez.streaming
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import com.axelfernandez.streaming.services.PlayerNotificationService
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.SimpleExoPlayer
@@ -14,7 +15,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
     }
 
 
